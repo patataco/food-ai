ccff53eebac1885fd6760fed2e1c3b75b6cc153e
Patata <patattta93@gmail.com>
2023-12-11 18:38:15 +0900

    feat: add the tRPC router for OpenAI communication, parse responses, and save to DB

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,49 +1,11 @@
-import { TRPCError } from '@trpc/server';
-import OpenAI from 'openai';
-import * as process from 'process';
 import { z } from 'zod';
 
-import { DishParams } from '~/models';
 import {
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
 } from '~/server/api/trpc';
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-export const aiRouter = createTRPCRouter({
-  generateText: protectedProcedure
-    .input(DishParams)
-    .mutation(async ({ ctx, input }) => {
-      const prompt = JSON.stringify(input);
-
-      try {
-        const completion: OpenAI.Chat.Completions.ChatCompletion =
-          await openai.chat.completions.create({
-            messages: [{ role: 'user', content: prompt }],
-            model: 'gpt-3.5-turbo',
-          });
-        const generatedText = completion.choices[0]?.message?.content ?? '';
-
-        await ctx.db.recipe.create({
-          data: {
-            ingredients: prompt,
-            recipe: generatedText,
-            createdBy: { connect: { id: ctx.session.user.id } },
-          },
-        });
-
-        return generatedText;
-      } catch (e) {
-        throw new TRPCError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: 'something went wrongs',
-        });
-      }
-    }),
-});
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
