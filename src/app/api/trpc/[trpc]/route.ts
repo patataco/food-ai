81448179f31f8b536f6a377a48141574d59e9978
Patata <patattta93@gmail.com>
2024-01-11 01:25:59 +0900

    fix: change max duration to 300:

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -5,7 +5,7 @@ import { env } from '~/env.mjs';
 import { appRouter } from '~/server/api/root';
 import { createTRPCContext } from '~/server/api/trpc';
 
-export const maxDuration = 30;
+export const maxDuration = 300;
 
 const handler = (req: NextRequest) =>
   fetchRequestHandler({
